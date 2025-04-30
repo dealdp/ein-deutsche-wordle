d8968a6d56f26c1531c644923abf7d6b8b628c4b
dealdp <dealdp@washjeff.edu>
2025-04-30 15:55:47 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// It's a shameless wordle clone
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
